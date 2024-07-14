224bb51943007201f08c0b6b43fccee17dde056a
lap02067 <lap02067@192.168.1.3>
2024-07-14 09:24:09 +0700

    Commit all file after 5th class

---
+++ Maven_prj_01/src/test/java/webdriver/BTVN_02_DragAndDrop.java
@@ -0,0 +1,45 @@
+import static org.testng.AssertJUnit.assertEquals;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import dev.failsafe.internal.util.Assert;
+
+public class BTVN_02_DragAndDrop {
+	WebDriver driver;
+
+	@BeforeClass
+	public void beforeClass() {
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+		driver.manage().window().maximize();
+	}
+
+	@Test
+	public void Drag_n_Drop() throws InterruptedException {
+		//Mở link
+		driver.get("https://automationfc.github.io/kendo-drag-drop/");
+		
+		//Action
+        WebElement smallcicle = driver.findElement(By.id("draggable"));
+        WebElement bigcicle = driver.findElement(By.id("droptarget"));
+        Actions action = new Actions(driver);
+        action.dragAndDrop(smallcicle, bigcicle).perform();
+        Thread.sleep(3000);
+        
+        //Check kq
+        //Assert.assertEquals("You did great!", bigcicle.getText());
+	}
+	
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+}
---
+++ Maven_prj_01/src/test/java/webdriver/Topic_04_selenium_api.java
@@ -0,0 +1,60 @@
+
+package webdriver;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_04_selenium_api {
+	WebDriver driver;
+
+	@BeforeClass
+	public void beforeClass() {
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().window().maximize();
+	}
+
+	public void TC_01_url() {
+		// Mở tab
+		driver.get("https://tiki.vn");
+
+		// lấy title
+		System.out.println(driver.getCurrentUrl());
+		System.out.println(driver.getTitle());
+		// đóng tab
+		driver.close();
+	}
+
+	@AfterClass
+	public void afterClass() {
+		// đóng browser
+		driver.quit();
+	}
+
+	@Test
+	public void TC_02_textbox() {
+		// Mở 1 page
+		driver.get("https://demo.nopcommerce.com/login?");	
+		
+		//viết vào field email
+		driver.findElement(By.xpath("//input[@id='Email']")).sendKeys("abc@gmail.com");
+		sleepInSeconds(5);
+		driver.navigate()
+		
+
+		//đóng tab
+		driver.close();
+	}
+
+	@AfterClass
+	public void afterClass() {
+		// đóng browser
+		driver.quit();
+	}
+}
